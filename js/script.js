367e42f13ed8f6343ff2dc07719ad1f7b11c95f5
Cix8 <francesco.natal_fn@libero.it>
2022-04-10 16:48:36 +0200

    structure fixed; our snake CAN CRAWL!!!

--- js/script.js
+++ js/script.js
@@ -1,17 +1,64 @@
 let snake;
+let a = 1;
+let b = 1;
 let nextStep;
 const rightBtn = document.getElementById('right-btn');
 const leftBtn = document.getElementById('left-btn');
+const downBtn = document.getElementById('down-btn');
+const upBtn = document.getElementById('up-btn');
 
 rightBtn.addEventListener ('click',
     function () {
-        snake = document.querySelector('.snake');
-        console.log(snake);
+        if (b<20) {
+            ++b;
+            snake = document.querySelector('.snake')
+            nextStep = document.querySelector('#row-'+a+' .col-'+b);
+            console.log(nextStep);
 
-        nextStep = document.querySelector('.snake+.col');
-        console.log(nextStep);
+            snake.classList.remove('snake');
+            nextStep.classList.add('snake');
+        }
+    }
+)
 
-        snake.classList.remove('snake');
-        nextStep.classList.add('snake')
+leftBtn.addEventListener ('click',
+    function () {
+        if (b>1) {
+            --b;
+            snake = document.querySelector('.snake')
+            nextStep = document.querySelector('#row-'+a+' .col-'+b);
+            console.log(nextStep);
+
+            snake.classList.remove('snake');
+            nextStep.classList.add('snake');
+        }
+    }
+)
+
+downBtn.addEventListener ('click',
+    function () {
+        if (a<20) {
+            ++a;
+            snake = document.querySelector('.snake')
+            nextStep = document.querySelector('#row-'+a+' .col-'+b);
+            console.log(nextStep);
+    
+            snake.classList.remove('snake');
+            nextStep.classList.add('snake');
+        }
+    }
+)
+
+upBtn.addEventListener ('click',
+    function () {
+        if (a>1) {
+            --a;
+            snake = document.querySelector('.snake')
+            nextStep = document.querySelector('#row-'+a+' .col-'+b);
+            console.log(nextStep);
+    
+            snake.classList.remove('snake');
+            nextStep.classList.add('snake');
+        }
     }
 )
